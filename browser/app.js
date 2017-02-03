61aa4bfe2734e8b1c301cf30bd00525c511c0a7c
John Pressman <jmpressman@gmail.com>
2017-02-03 13:07:29 -0500

    finished main workshop

---
+++ browser/app.js
@@ -0,0 +1,17 @@
+// Never seen window.location before?
+// This object describes the URL of the page we're on!
+
+var socket = io(window.location.origin);
+
+socket.on('connect', function () {
+    console.log('I have made a persistent two-way connection to the server!');
+});
+
+socket.on('drawn', function(start, end, strokeColor) {
+  whiteboard.draw(start, end, strokeColor, false);
+});
+
+whiteboard.on('draw', function(start, end, strokeColor) {
+  console.log(strokeColor);
+  socket.emit('draw', start, end, strokeColor);
+});
